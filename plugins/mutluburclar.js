915790dd19448ca42979d5fd428db7b01b518212
Çağan Özdemir <82602282+asenablack@users.noreply.github.com>
2021-04-18 03:00:24 +0300

    Delete mutluburclar.js

--- plugins/mutluburclar.js
+++
@@ -1,5 +0,0 @@
-const Asena = require('../events');
-const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
-
-    Asena.addCommand({pattern: 'mutluburclar', fromMe: true, deleteCommand: false}, (async (message, match) => {
-            await message.sendMessage('Bu Hafta Mutlu Olacak Burçlar;\n\n- Akrep\n-Balık\n-Aslan\n-Başak\n-Kova\n-Oğlak');
