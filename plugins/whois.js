2a00585ca1d0927b020b97b2624d9ce1d04a2fc3
#DevFelix <82602282+asenablack@users.noreply.github.com>
2021-04-20 04:03:29 +0300

    Update whois.js

--- plugins/whois.js
+++ plugins/whois.js
@@ -6,16 +6,52 @@ const das = "Grup metada verisini çeker."
 
 Asena.addCommand({ pattern: 'whois', fromMe: true, desc: das }, async (message, match) => { 
 
-    const ppUrl = await message.client.getProfilePicture(message.jid) 
+    if (message.jid.includes('-')) {
+        var json = await message.client.groupMetadataMinimal(message.jid) 
 
-    const json = await message.client.groupMetadata(message.jid) 
+        var code = await message.client.groupInviteCode(message.jid)
 
-    const resim = await dil.get(ppUrl, {responseType: 'arraybuffer'})
+        var nwjson = await message.client.groupMetadata(message.jid) 
 
-    await message.client.sendMessage(
-        Buffer.from(resim.data), 
-        MessageType.image, 
-        { caption: "*Group ID:* " + json.id + "\n*Group Name:* " + json.subject + "\n*Description:* " + json.desc }
-    );
+        const msg = `*Grup ID:* ${json.id} \n*Grup İsmi:* ${nwjson.subject} \n*Kurucu:* ${json.owner} \n*Grup Kodu:* ${code} \n*Grup Açıklaması:* \n\n${nwjson.desc}`
 
+        var ppUrl = await message.client.getProfilePicture(message.jid) 
+
+        const resim = await dil.get(ppUrl, {responseType: 'arraybuffer'})
+
+        await message.sendMessage(
+            Buffer.from(resim.data), 
+            MessageType.image, 
+            { caption: msg }
+        );
+    }
+    else {
+        var status = await message.client.getStatus(message.jid) 
+        var usppUrl = await message.client.getProfilePicture(message.jid) 
+        var usexists = await message.client.isOnWhatsApp(message.jid)
+        const nwmsg = `*Kişi JID:* ${usexists.jid} \n*Kişi Durumu:* ${status.status}`
+        const resimnw = await dil.get(usppUrl, {responseType: 'arraybuffer'})
+        await message.sendMessage(
+            Buffer.from(resimnw.data), 
+            MessageType.image, 
+            { caption: nwmsg }
+        );
+    }
 });
+const tg = "Tag All!"
+const ad = "Sessizce Herkesi Etiketler"
+
+Asena.addCommand({ pattern: 'af', fromMe: true, desc: ad}, (async (message, match) => { 
+
+    grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(
+        async (uye) => {
+            mesaj += '@' + uye.id.split('@')[0] + ' ';
+            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            
+        }
+    );
+    await message.client.sendMessage(message.jid,tg, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+}));
